0d0d8cedad1eafb0f38ca0ab7a3021d8e7febd62
Rijuldeepsingh <rijualbagga02@gmail.com>
2025-03-02 21:22:44 +0530

    Initial commit

---
+++ src/dsa/stack.java
@@ -0,0 +1,31 @@
+package dsa;
+
+import java.util.Stack;
+
+public class stack {
+	public static void main(String[] args) {
+		Stack<String> stack  = new Stack<String>();
+		
+		//this is push method
+		stack.push("a");
+		stack.push("b");
+		stack.push("c");
+		stack.push("d");
+		stack.push("e");
+		
+		
+		
+		//stack.pop(); this is pop method
+		
+		String fav = stack.pop();
+		
+		System.out.println(stack);
+		System.out.println(fav);
+		
+		//to check the top most element we use peek method
+		System.out.println(stack.peek());
+		
+		//to search we use search method
+		System.out.println(stack.search("c"));
+	}
+}
